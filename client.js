978c6f5513dfa1f1150b0270080a012cb812b789
Miranda Zheng <zhengshuang2011@gmail.com>
2021-12-02 02:07:52 +0000

    Implement some special keys that send canned messages to the server

--- client.js
+++ client.js
@@ -1,5 +1,9 @@
 const net = require("net");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+});
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
@@ -13,6 +17,10 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: ZM");
+  });
+
+  rl.on("line", (input) => {
+    conn.write(`Say: ${input}`);
   });
 
   return conn;
